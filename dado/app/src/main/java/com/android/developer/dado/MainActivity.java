9769e8460e50763cefab09b83abbf46e3b89c078
SERVIDORES\rodrigo.rufino <rodrigo.rufino@inatel.br>
2017-01-09 16:03:57 -0200

    Ícone e Overflow Menu

--- dado/app/src/main/java/com/android/developer/dado/MainActivity.java
+++ dado/app/src/main/java/com/android/developer/dado/MainActivity.java
@@ -1,10 +1,14 @@
 package com.android.developer.dado;
 
+import android.content.DialogInterface;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener{
@@ -23,6 +27,38 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
         toolbar.setTitle("Dice of Power");
 
         configToolbar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_about: {
+                menuAboutDialog();
+                break;
+            }
+            // case blocks for other MenuItems (if any)
+        }
+        return false;
+    }
+
+    private void menuAboutDialog(){
+        AlertDialog.Builder readyDialog = new AlertDialog.Builder(MainActivity.this);
+        readyDialog.setTitle("About");
+        readyDialog.setMessage("Created by Rodrigo R. Ribeiro\n\n\n\n\nv1.0\n\nJanuary, 2017").setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //função do botão de OK
+                    }
+                });
+        AlertDialog dialog = readyDialog.create();
+        dialog.show();
     }
 
     @Override
